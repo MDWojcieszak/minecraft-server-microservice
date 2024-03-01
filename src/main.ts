091efdf63889b8eac3f6788768b6c96623246e1e
MDWojcieszak <wojcieszak.4@gmail.com>
2024-03-01 13:52:17 +0100

    feature(main): global class validation

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { config } from './config/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
@@ -11,6 +12,8 @@ async function bootstrap() {
       options: config().rabbitMQConfig,
     },
   );
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
   await app.listen();
 }
 bootstrap();
