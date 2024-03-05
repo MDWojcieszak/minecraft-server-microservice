6af51ec8b67eac92fe5cef22dbd1ca6723449c4d
MDWojcieszak <wojcieszak.4@gmail.com>
2024-03-05 23:53:16 +0100

    feature(systemUsage): system usage module

--- src/system-usage/system-usage.controller.ts
+++ src/system-usage/system-usage.controller.ts
@@ -5,9 +5,4 @@ import { SystemUsageService } from 'src/system-usage/system-usage.service';
 @Controller('system-usage')
 export class SystemUsageController {
   constructor(private systemUsageService: SystemUsageService) {}
-
-  @MessagePattern('get_system_usage')
-  async getSystemUsage() {
-    return await this.systemUsageService.getSystemUsage();
-  }
 }
--- src/system-usage/system-usage.service.ts
+++ src/system-usage/system-usage.service.ts
@@ -1,22 +1,85 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import * as si from 'systeminformation';
 
 @Injectable()
 export class SystemUsageService {
-  async getSystemUsage() {
+  constructor(private config: ConfigService) {}
+  async getSystemLoad() {
     try {
-      const cpuData = await si.cpu();
-      const memData = await si.mem();
-      const diskData = await si.fsSize();
-      console.log(cpuData, memData, diskData);
-      return {
-        cpu: cpuData,
-        memory: memData,
-        disk: diskData,
-      };
+      const load = await si.currentLoad();
+      return load;
     } catch (error) {
       console.error('Error retrieving system information:', error);
       throw error;
+    }
+  }
+
+  async getMemoryInfo() {
+    try {
+      const memData = await si.mem();
+      return memData;
+    } catch (error) {
+      console.error('Error retrieving system information:', error);
+      throw error;
+    }
+  }
+
+  async getCpuInfo() {
+    try {
+      const cpuData = await si.cpu();
+      return cpuData;
+    } catch (error) {
+      console.error('Error retrieving system information:', error);
+      throw error;
+    }
+  }
+
+  async getSystemUptime() {
+    try {
+      const timeInfo = si.time();
+      const uptime = Math.floor(timeInfo.uptime);
+      return uptime;
+    } catch (error) {
+      console.error('Error retrieving system uptime:', error);
+      throw error;
+    }
+  }
+  async getDiskInfo() {
+    try {
+      const diskData = await si.fsSize();
+      return diskData;
+    } catch (error) {
+      console.error('Error retrieving system information:', error);
+      throw error;
+    }
+  }
+
+  async getPublicIP() {
+    try {
+      const networkInterfaces = await si.networkInterfaces();
+      let publicInterface: si.Systeminformation.NetworkInterfacesData = null;
+
+      if (Array.isArray(networkInterfaces)) {
+        publicInterface = networkInterfaces.find((i) => {
+          return i.ip4 && i.ip4 !== '127.0.0.1' && i.ip4 !== '::1';
+        });
+      } else if (
+        networkInterfaces &&
+        networkInterfaces.ip4 &&
+        networkInterfaces.ip4 !== '127.0.0.1' &&
+        networkInterfaces.ip4 !== '::1'
+      ) {
+        publicInterface = networkInterfaces;
+      }
+
+      if (publicInterface) {
+        return publicInterface.ip4;
+      } else {
+        throw new Error('Public IP address not found.');
+      }
+    } catch (error) {
+      throw new Error(error);
     }
   }
 }
