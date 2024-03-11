1e3cc4a4d8fa24956d6919f39639aa748a1b7d95
MDWojcieszak <wojcieszak.4@gmail.com>
2024-03-11 23:57:53 +0100

    feature(main): connect modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,27 @@
 import { Module } from '@nestjs/common';
-import { MinecraftServerService } from './minecraft-server/minecraft-server.service';
 import { MinecraftServerModule } from './minecraft-server/minecraft-server.module';
 import { ConfigModule } from '@nestjs/config';
 import { config } from './config/config';
 import { SystemUsageModule } from './system-usage/system-usage.module';
 import { CronJobsModule } from 'src/cron-jobs/cron-jobs.module';
+import { SystemRegisterModule } from './system-register/system-register.module';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+import { CommandModule } from 'src/command/command.module';
+import { ServerSettingsModule } from './server-settings/server-settings.module';
 
 @Module({
   imports: [
     MinecraftServerModule,
+    EventEmitterModule.forRoot(),
     ConfigModule.forRoot({
       isGlobal: true,
       load: [config],
     }),
     SystemUsageModule,
     CronJobsModule,
+    SystemRegisterModule,
+    CommandModule,
+    ServerSettingsModule,
   ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { config } from './config/config';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe } from '@nestjs/common/pipes';
 
 async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
@@ -12,6 +12,7 @@ async function bootstrap() {
       options: config().rabbitMQConfig,
     },
   );
+  app.enableShutdownHooks();
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
   await app.listen();
