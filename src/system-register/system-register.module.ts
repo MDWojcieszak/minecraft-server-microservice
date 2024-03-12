2769194340db67cd0b2a6c306b6ef9bb9c064627
MDWojcieszak <wojcieszak.4@gmail.com>
2024-03-12 22:14:51 +0100

    feature(systemRegister): register commands

--- src/system-register/system-register.module.ts
+++ src/system-register/system-register.module.ts
@@ -3,9 +3,11 @@ import { SystemRegisterService } from './system-register.service';
 import { SystemRegisterController } from './system-register.controller';
 import { HubConnectionModule } from 'src/hub-connection/hub-connection.module';
 import { SystemUsageModule } from 'src/system-usage/system-usage.module';
+import { CommandCollectorSerice } from 'src/command/command-collector.service';
+import { CommandModule } from 'src/command/command.module';
 
 @Module({
-  imports: [HubConnectionModule, SystemUsageModule],
+  imports: [HubConnectionModule, SystemUsageModule, CommandModule],
   providers: [SystemRegisterService],
   controllers: [SystemRegisterController],
   exports: [SystemRegisterService],
--- src/system-register/system-register.service.ts
+++ src/system-register/system-register.service.ts
@@ -3,13 +3,17 @@ import { ConfigService } from '@nestjs/config';
 import { EventEmitter2 } from '@nestjs/event-emitter';
 import { ClientProxy } from '@nestjs/microservices';
 import { firstValueFrom } from 'rxjs';
+import { CommandCollectorSerice } from 'src/command/command-collector.service';
 import { ServerStatus } from 'src/common/enums';
+import { RegisterServerCommandsEvent } from 'src/system-register/events';
+import { RegisterServerEvent } from 'src/system-register/events/register-server.event';
 import { SystemUsageService } from 'src/system-usage/system-usage.service';
 
 @Injectable()
 export class SystemRegisterService {
   constructor(
     private config: ConfigService,
+    private commandCollector: CommandCollectorSerice,
     private systemUsage: SystemUsageService,
     private eventEmmiter: EventEmitter2,
     @Inject('HUB') private hubClient: ClientProxy,
@@ -24,16 +28,31 @@ export class SystemRegisterService {
       const ipAddress = await this.systemUsage.getPublicIP();
       const cpu = await this.systemUsage.getCpuInfo();
       await firstValueFrom(
-        this.hubClient.send('register-server', {
-          name: serverName,
-          ipAddress,
-          diskCount,
-          cpu,
-        }),
+        this.hubClient.send(
+          'register-server',
+          new RegisterServerEvent(serverName, ipAddress, diskCount, cpu),
+        ),
       );
+      await this.handleCommandRegistration(serverName);
       this.eventEmmiter.emit('system.registered');
     } catch (e) {
       Logger.error('REGISTRATION FAILED');
+    }
+  }
+
+  async handleCommandRegistration(serverName: string) {
+    try {
+      Logger.log('COMMANDS REGISTRATION');
+      const commands = this.commandCollector.getAll();
+
+      await firstValueFrom(
+        this.hubClient.send(
+          'register-commands',
+          new RegisterServerCommandsEvent(serverName, commands),
+        ),
+      );
+    } catch (e) {
+      Logger.error('COMMAND_REGISTRATION_FAILED');
     }
   }
 
