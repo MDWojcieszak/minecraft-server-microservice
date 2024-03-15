8b42d15feb4f4c0a679aed9147aa4ac0112a3637
MDWojcieszak <wojcieszak.4@gmail.com>
2024-03-15 22:44:58 +0100

    feature(minecraftServer): command decorators

--- src/minecraft-server/minecraft-server.controller.ts
+++ src/minecraft-server/minecraft-server.controller.ts
@@ -8,11 +8,15 @@ import {
 import { StartServerDto } from 'src/minecraft-server/dto/start-server.dto';
 import { SendCommandDto } from 'src/minecraft-server/dto';
 import { validate } from 'class-validator';
+import { OnEvent } from '@nestjs/event-emitter';
+import { Command } from 'src/common/decorators';
+import { CommandType, ServerCategory } from 'src/common/enums';
 
 @Controller('minecraft-server')
 export class MinecraftServerController {
   constructor(private minecrafService: MinecraftServerService) {}
 
+  @Command('start_server', ServerCategory.MINECRAFT_SERVER, CommandType.MESSAGE)
   @MessagePattern('start_server')
   async startServer(dto: StartServerDto) {
     const errors = await validate(dto);
@@ -22,11 +26,13 @@ export class MinecraftServerController {
     return this.minecrafService.startMinecraftServer(dto);
   }
 
+  @Command('stop_server', ServerCategory.MINECRAFT_SERVER, CommandType.MESSAGE)
   @MessagePattern('stop_server')
   stopServer() {
     return this.minecrafService.stopMinecraftServer();
   }
 
+  @Command('send_command', ServerCategory.MINECRAFT_SERVER, CommandType.EVENT)
   @EventPattern('send_command')
   async sendCommand(dto: SendCommandDto) {
     const errors = await validate(dto);
